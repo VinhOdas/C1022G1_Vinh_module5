8f304d47915646ff219d2f274238a135eed58530
Your Name <you@example.com>
2023-03-31 09:43:14 +0700

    fix loi

--- ss2_react/element-students/src/index.js
+++ ss2_react/element-students/src/index.js
@@ -1,66 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const students = [
-  {
-    company: 'Alfreds Futterkiste',
-    contact: 'Maria Anders',
-    country: 'Germany'
-  },
-  {
-    company: 'Centro comercial Moctezuma',
-    contact: 'Francisco Chang',
-    country: 'Mexico'
-  },
-  {
-    company: 'Ernst Handel',
-    contact: 'Roland Mendel',
-    country: 'Austria'
-  },
-  {
-    company: 'Island Trading',
-    contact: 'Helen Bennett',
-    country: 'UK'
-  },
-  {
-    company: 'Laughing Bacchus Winecellars',
-    contact: 'Yoshi Tannamuri',
-    country: 'Canada'
-  },
-  {
-    company: 'Magazzini Alimentari Riuniti',
-    contact: 'Giovanni Rovelli',
-    country: 'Italy'
-  }
-]
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div style={{width: "100%"}}>
-      <table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>company</th>
-          <th>contact</th>
-          <th>country</th>
-        </tr>
-      </thead>
-     
-    {
-      students.map((student,index) =>(
-       <tbody>
-        <tr>
-          <td>{index + 1}</td>
-          <td>{student.company}</td>
-          <td>{student.contact}</td>
-          <td>{student.country}</td>
-        </tr>
-        </tbody>
-      ))
-    }
-     </table>
-  </div>
+  
+  <React.StrictMode>
+  <App />
+</React.StrictMode>
 
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
