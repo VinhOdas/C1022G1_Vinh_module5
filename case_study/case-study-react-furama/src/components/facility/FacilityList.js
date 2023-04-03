78653b918c9c6739c0ddab95c64d48ff3f365d30
Your Name <you@example.com>
2023-04-03 19:18:26 +0700

    fix cases

--- case_study/case-study-react-furama/src/components/facility/FacilityList.js
+++ case_study/case-study-react-furama/src/components/facility/FacilityList.js
@@ -1,128 +1,76 @@
 import React from 'react'
 import Footer from '../home/Footer'
 import Navigation from '../home/Navigation'
-<<<<<<< HEAD
+import facilityData from './model/Facility'
 
-class FacilityList extends Component {
-  render() {
-    return (
+function FacilityList() {
+  return (
 
-      <div>
-        <Header />
-        <Navigation />
-        <div className="container-fluid">
-          <div>
-            <button className="btn btn-primary btn-outline-secondary btn-sm" data-bs-toggle="modal"
-              data-bs-target="#addContract">
-              <span className="fa-solid fa-plus text-light h6 my-auto me-1"></span>
-              <span className="text-light"> Create Facility</span>
-            </button>
-          </div>
-          <div className="row">
-            <table className="table table-dark" border="1">
-              <thead>
-                <tr>
-                  <th>STT</th>
-                  <th>Name</th>
-                  <th>Area</th>
-                  <th>Cost</th>
-                  <th>Max People</th>
-                  <th>Standard Room</th>
-                  <th>Description Other Convenience</th>
-                  <th>Pool Area</th>
-                  <th>Floors</th>
-                  <th>Facility Free</th>
-                  <th>Rent Type</th>
-                  <th>Facility Type</th>
-                  <th>Edit</th>
-                  <th>Delete</th>
-                </tr>
-              </thead>
-              <tbody align="center">
-                <tr></tr>
-              </tbody>
-            </table>
+    <div>
+      <Navigation />
+      <div className="container-fluid">
+        <div>
+          <button className="btn btn-primary btn-outline-secondary btn-sm" data-bs-toggle="modal"
+            data-bs-target="#addContract">
+            <span className="fa-solid fa-plus text-light h6 my-auto me-1"></span>
+            <span className="text-light"> Create Facility</span>
+          </button>
+        </div>
+        <div className="row">
+          <table className="table table-dark" border="1" style={{ textAlign: 'center' }}>
+            <thead>
+              <tr>
+                <th>STT</th>
+                <th>Name</th>
+                <th>Area</th>
+                <th>Cost</th>
+                <th>Max People</th>
+                <th>Standard Room</th>
+                <th>Description Other Convenience</th>
+                <th>Pool Area</th>
+                <th>Floors</th>
+                <th>Facility Free</th>
+                <th>Rent Type</th>
+                <th>Facility Type</th>
+                <th>Edit</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody align="center">
 
-          </div>
 
+              {
+                facilityData.facility.map((facilityLists, index) => (
+                  <>
+                    <tr key={facilityLists.id}>
+                      <td key={facilityLists.id} >{index + 1}</td>
+                      <td>{facilityLists.name}</td>
+                      <td>{facilityLists.area}</td>
+                      <td>{facilityLists.cost}</td>
+                      <td>{facilityLists.maxPeople}</td>
+                      <td>{facilityLists.standardRoom}</td>
+                      <td>{facilityLists.descriptionOtherCovenience}</td>
+                      <td>{facilityLists.poolArea}</td>
+                      <td>{facilityLists.numberOfFloors}</td>
+                      <td>{facilityLists.facilityFree}</td>
+                      <td>{facilityLists.rentType.name}</td>
+                      <td>{facilityLists.facilityType.name}</td>
+                    </tr>
+                  </>
+
+                ))
+              }
+
+            </tbody>
+          </table>
 
         </div>
-=======
-import facilityData from './model/Facility'
- 
- function FacilityList()  {
-    return (
-   
-        <div>
-
-<Navigation/>
-<div className="container-fluid">
-  <div>
-    <button className="btn btn-primary btn-outline-secondary btn-sm" data-bs-toggle="modal"
-            data-bs-target="#addContract">
-      <span className="fa-solid fa-plus text-light h6 my-auto me-1"></span>
-      <span   className="text-light"> Create Facility</span>
-    </button>
-  </div>
-  <div className="row">
-    <table className="table table-dark" border="1" style={{textAlign: 'center'}}>
-      <thead>
-      <tr>
-        <th>STT</th>
-        <th>Name</th>
-        <th>Area</th>
-        <th>Cost</th>
-        <th>Max People</th>
-        <th>Standard Room</th>
-        <th>Description Other Convenience</th>
-        <th>Pool Area</th>
-        <th>Floors</th>
-        <th>Facility Free</th>
-        <th>Rent Type</th>
-        <th>Facility Type</th>
-        <th>Edit</th>
-        <th>Delete</th>
-      </tr>
-      </thead>
-      <tbody align="center">
-
-    
-        {
-          facilityData.facility.map((facilityLists,index) =>(
-            <>
-             <tr key={facilityLists.id}>
-              <td  key={facilityLists.id} >{index + 1}</td>
-              <td>{facilityLists.name}</td>
-              <td>{facilityLists.area}</td>
-              <td>{facilityLists.cost}</td>
-              <td>{facilityLists.maxPeople}</td>
-              <td>{facilityLists.standardRoom}</td>
-              <td>{facilityLists.descriptionOtherCovenience}</td>
-              <td>{facilityLists.poolArea}</td>
-              <td>{facilityLists.numberOfFloors}</td>
-              <td>{facilityLists.facilityFree}</td>
-              <td>{facilityLists.rentType.name}</td>
-              <td>{facilityLists.facilityType.name}</td>
-              </tr>
-              </>
-              
-          ))
-        }
-     
-      </tbody>
-    </table>
-
-  </div>
-</div>
-<Footer/>
-       </div>
->>>>>>> cc711da36ace554ba730b1ffb4ef80988b9c00f4
-
-        <Footer />
-
       </div>
+      <Footer />
+    </div>
 
-    )
-  }
+
+  )
+}
 
 export default FacilityList
